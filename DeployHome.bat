31412e98edd40b12e5c1c7717268d290b36a36cb
Andrey Gusarov <AGusarov@live.ag>
2022-08-18 02:05:20 +0300

    Форма регистрации кластера

--- DeployHome.bat
+++ DeployHome.bat
@@ -11,5 +11,8 @@ ibcmd infobase create --data="%tempdir%\fs-data" --db-path="%tempdir%\file-db\db
 ibcmd infobase config import --data="%tempdir%\fs-data" --db-path="%tempdir%\file-db\db-data" %repository%
 ibcmd infobase config save --data="%tempdir%\fs-data" --db-path="%tempdir%\file-db\db-data" %tempdir%\1Cv8.cf
 
+rem Выгрузка базы в репозиторий
+"C:\Program Files\1cv8\8.3.20.1613\bin\1cv8.exe" DESIGNER /F"G:\1C_Bases\Union_3" /N"Администратор" /DumpConfigToFiles "G:\_Git\Nexus" -update /DisableStartupDialogs /DisableStartupMessages
 
-"C:\Program Files\1cv8\8.3.20.1613\bin\1cv8.exe" DESIGNER /DumpConfigToFiles "G:\_Git\Nexus" -update
+rem Загрузка базы из репозитория
+"C:\Program Files\1cv8\8.3.20.1613\bin\1cv8.exe" DESIGNER /F"G:\1C_Bases\Union_3" /N"Администратор" /LoadConfigFromFiles "G:\_Git\Nexus" -NoCheck /UpdateDBCfg /DisableStartupDialogs /DisableStartupMessages
