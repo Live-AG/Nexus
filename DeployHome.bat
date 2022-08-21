bfed59b2adb42cb48050788db9787c6e2d0ec717
Andrey Gusarov <AGusarov@live.ag>
2022-08-22 02:59:24 +0300

    Длительные операции с кластером

--- DeployHome.bat
+++ DeployHome.bat
@@ -11,8 +11,9 @@ ibcmd infobase create --data="%tempdir%\fs-data" --db-path="%tempdir%\file-db\db
 ibcmd infobase config import --data="%tempdir%\fs-data" --db-path="%tempdir%\file-db\db-data" %repository%
 ibcmd infobase config save --data="%tempdir%\fs-data" --db-path="%tempdir%\file-db\db-data" %tempdir%\1Cv8.cf
 
-rem Выгрузка базы в репозиторий
-"C:\Program Files\1cv8\8.3.20.1613\bin\1cv8.exe" DESIGNER /F"G:\1C_Bases\Union_3" /N"Администратор" /DumpConfigToFiles "G:\_Git\Nexus" -update /DisableStartupDialogs /DisableStartupMessages
-
 rem Загрузка базы из репозитория
-"C:\Program Files\1cv8\8.3.20.1613\bin\1cv8.exe" DESIGNER /F"G:\1C_Bases\Union_3" /N"Администратор" /LoadConfigFromFiles "G:\_Git\Nexus" -NoCheck /UpdateDBCfg /DisableStartupDialogs /DisableStartupMessages
+"C:\Program Files\1cv8\common\1cestart.exe" DESIGNER /F"G:\1C_Bases\Union_3" /N"Администратор" /LoadConfigFromFiles "G:\_Git\Nexus" -NoCheck /UpdateDBCfg /DisableStartupDialogs /DisableStartupMessages
+
+rem Выгрузка базы в репозиторий
+"C:\Program Files\1cv8\common\1cestart.exe" DESIGNER /F"G:\1C_Bases\Union_3" /N"Администратор" /DumpConfigToFiles "G:\_Git\Nexus" -update /DisableStartupDialogs /DisableStartupMessages
+
