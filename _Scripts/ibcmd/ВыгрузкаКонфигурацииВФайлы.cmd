7d6e61ef9f893e40a2ddd3058d50d8d36ed0e7d0
Andrey Gusarov <AGusarov@live.ag>
2022-11-22 23:55:06 +0300

    Параметры скрипта

--- _Scripts/ibcmd/ВыгрузкаКонфигурацииВФайлы.cmd
+++ _Scripts/ibcmd/ВыгрузкаКонфигурацииВФайлы.cmd
@@ -1,3 +1,8 @@
 
 
+set workdir="G:\Temp\ShipLoading\"
+set appdir="C:\Program Files\1cv8\8.3.20.1613\bin\"
+
+"%appdir%ibcmd" infobase config export objects --data="%workdir%Data" --config="%workdir%config.yml" --user="Администратор" --password="" --out="%workdir%src" "Configuration"
+
 "C:\Program Files\1cv8\8.3.20.1613\bin\ibcmd" infobase config export objects --data="G:\Temp\ShipLoading\Data" --config="G:\Temp\ShipLoading\config.yml" --user="Администратор" --password="" --out="G:\Temp\ShipLoading\src" "Configuration"
